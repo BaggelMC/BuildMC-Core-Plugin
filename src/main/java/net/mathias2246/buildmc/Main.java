ac4b68780fd4e282410a7cb8b53acebefc36f7c9
Mathias2246 <mathias123singer@gmail.com>
2025-07-28 15:17:28 +0200

    First Command

---
+++ src/main/java/net/mathias2246/buildmc/Main.java
@@ -0,0 +1,17 @@
+package net.mathias2246.buildmc;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
