bdbdba3faf084079cd16332eca85df5bc8dfd760
Darkylt <darkprankingyt@gmail.com>
2025-08-01 17:00:43 +0200

    Fix /claim who command

--- paper/src/main/java/net/mathias2246/buildmc/claims/ClaimCommand.java
+++ paper/src/main/java/net/mathias2246/buildmc/claims/ClaimCommand.java
@@ -8,10 +8,12 @@ import io.papermc.paper.command.brigadier.argument.resolvers.selector.PlayerSele
 import net.kyori.adventure.text.Component;
 import net.mathias2246.buildmc.Main;
 import net.mathias2246.buildmc.commands.CustomCommand;
+import net.mathias2246.buildmc.util.Message;
 import org.bukkit.entity.Player;
 import org.bukkit.scoreboard.Team;
 
 import java.io.IOException;
+import java.util.Map;
 import java.util.Objects;
 
 import static net.mathias2246.buildmc.Main.claimManager;
@@ -40,7 +42,16 @@ public class ClaimCommand implements CustomCommand {
                                         command.getSource().getSender().sendMessage(Component.translatable("messages.error.not-a-player"));
                                         return 0;
                                     }
-                                    command.getSource().getSender().sendMessage(Objects.requireNonNull(ClaimManager.getOwnerString(command.getSource().getLocation())));
+
+                                    String owner = ClaimManager.getOwnerString(command.getSource().getLocation());
+
+                                    if (owner == null) {
+                                        command.getSource().getSender().sendMessage(Component.translatable("messages.claims.who.unclaimed"));
+                                        return 1;
+                                    }
+
+                                    Component message = Message.msg(player, "messages.claims.who.message", Map.of("owner", owner));
+                                    command.getSource().getSender().sendMessage(message);
                                     return 1;
                                 }
                         )
