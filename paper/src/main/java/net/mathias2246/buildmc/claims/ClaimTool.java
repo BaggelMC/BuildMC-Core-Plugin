041133293935df5bb38db6bc5893320568e51df5
Darkylt <darkprankingyt@gmail.com>
2025-08-01 15:46:10 +0200

    Updating more messages

--- paper/src/main/java/net/mathias2246/buildmc/claims/ClaimTool.java
+++ paper/src/main/java/net/mathias2246/buildmc/claims/ClaimTool.java
@@ -21,7 +21,6 @@ import org.bukkit.scoreboard.Team;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
