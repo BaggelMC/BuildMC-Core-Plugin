cd9d4dd5722e038cbe56e15810470e87c3aa2f89
Darkylt <darkprankingyt@gmail.com>
2025-08-03 21:39:32 +0200

    Issue #29

--- spigot/src/main/java/net/mathias2246/buildmc/claims/ClaimPlaceListener.java
+++ spigot/src/main/java/net/mathias2246/buildmc/claims/ClaimPlaceListener.java
@@ -1,11 +1,13 @@
 package net.mathias2246.buildmc.claims;
 
 import net.kyori.adventure.text.Component;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.player.PlayerBucketEmptyEvent;
 
 import static net.mathias2246.buildmc.Main.audiences;
 import static net.mathias2246.buildmc.Main.claimManager;
@@ -17,6 +19,17 @@ public class ClaimPlaceListener implements Listener {
         Player player = event.getPlayer();
 
         if (!ClaimManager.isPlayerAllowed(claimManager, player, event.getBlock().getLocation())) {
+            audiences.player(player).sendActionBar(Component.translatable("messages.claims.not-accessible.block-place"));
+            event.setCancelled(true);
+        }
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onBucketEmpty(PlayerBucketEmptyEvent event) {
+        Player player = event.getPlayer();
+        Block block = event.getBlock();
+
+        if (!ClaimManager.isPlayerAllowed(claimManager, player, block.getLocation())) {
             audiences.player(player).sendActionBar(Component.translatable("messages.claims.not-accessible.block-place"));
             event.setCancelled(true);
         }
